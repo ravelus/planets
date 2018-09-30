26eb3c0547bb1fe4b7b3974d4b83fc0208170eda
Luis Rodríguez Neches <luis.rodriguez.neches@everis.com>
2018-09-30 13:07:09 -0300

    Add unit tests for weather forecast. Skeletons only

---
+++ PlanetSystemUnitTests/WeatherForecastTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PlanetSystem;
+namespace PlanetSystemUnitTests
+{
+    [TestClass]
+    public class WeatherForecastTests
+    {
+        [TestMethod]
+        public void IsDryTodayTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsWetTodayTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsVeryWetTodayTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void IsGreatTodayTest()
+        {
+
+        }
+    }
+}
