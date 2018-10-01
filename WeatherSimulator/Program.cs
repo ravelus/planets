727f789a070e52d784e70aac177240322670942f
Luis Rodríguez Neches <luis.rodriguez.neches@everis.com>
2018-10-01 16:17:39 -0300

    Small refactor in main program and return as soon as possible

--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using PlanetSystem.Forecast;
 using PlanetSystem.Models;
-
 
 namespace WeatherSimulator
 {
@@ -10,13 +10,6 @@ namespace WeatherSimulator
     {
         static void Main(string[] args)
         {
-            int totalDays = 10 * 365; // 10 years on the Earth
-
-            int dryDays = 0;
-            int wetDays = 0;
-            int perfectDays = 0;
-            IList<int> veryWetDaysRegister = new List<int>();
-
             Planet ferengi = new Planet(1, 500);
             Planet betasoide = new Planet(3, 2000);
             Planet vulcano = new Planet(5, 1000);
@@ -33,6 +26,17 @@ namespace WeatherSimulator
                 }
             };
 
+            int totalDays = 10 * 365; // 10 years on the Earth
+            RunAsync(system, totalDays);
+        }
+
+        static void RunAsync(StarSystem system, int totalDays)
+        {
+            int dryDays = 0;
+            int wetDays = 0;
+            int perfectDays = 0;
+            IList<int> veryWetDaysRegister = new List<int>();
+
             WeatherForecast forecaster = new WeatherForecast(system);
             for (int i = 1; i <= totalDays; i++) //let's start couting on day 1 :)
             {
@@ -41,21 +45,23 @@ namespace WeatherSimulator
                 if (forecaster.IsGreatToday())
                 {
                     perfectDays++;
+                    continue;
                 }
 
                 if (forecaster.IsDryToday())
                 {
                     dryDays++;
+                    continue;
                 }
 
-                if (forecaster.IsWetToday())
-                {
-                    wetDays++;
+                if (!forecaster.IsWetToday())
+                    continue;
 
-                    if (forecaster.IsVeryWetToday())
-                    {
-                        veryWetDaysRegister.Add(i);
-                    }
+                wetDays++;
+
+                if (forecaster.IsVeryWetToday())
+                {
+                    veryWetDaysRegister.Add(i);
                 }
             }
         }
