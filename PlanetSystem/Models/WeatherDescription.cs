a9fdfca73b982cca2b8437b402d5bff93c564f8d
Luis Rodríguez Neches <luis.rodriguez.neches@everis.com>
2018-10-07 18:52:57 -0300

    Complete the program, save the data into a file!

---
+++ PlanetSystem/Models/WeatherDescription.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanetSystem.Models
+{
+    public enum WeatherDescription
+    {
+        None = 0,
+        Dry = 1,
+        Wet = 2,
+        VeryWet = 3,
+        Great = 4
+    }
+}
--- WeatherSimulator/Program.cs
+++ WeatherSimulator/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 using PlanetSystem.Forecast;
 using PlanetSystem.Models;
@@ -30,38 +33,70 @@ namespace WeatherSimulator
 
         static void RunAsync(StarSystem system, int totalDays)
         {
-            int dryDays = 0;
-            int wetDays = 0;
-            int perfectDays = 0;
-            IList<int> veryWetDaysRegister = new List<int>();
+            var forecastByDay = new Dictionary<int, WeatherDescription>();
 
             WeatherForecast forecaster = new WeatherForecast(system);
             for (int i = 1; i <= totalDays; i++) //let's start couting on day 1 :)
             {
                 forecaster.Dawn();
 
+                forecastByDay.Add(i, WeatherDescription.None);
+
                 if (forecaster.IsGreatToday())
                 {
-                    perfectDays++;
+                    forecastByDay[i] = WeatherDescription.Great;
                     continue;
                 }
 
                 if (forecaster.IsDryToday())
                 {
-                    dryDays++;
+                    forecastByDay[i] = WeatherDescription.Dry;
                     continue;
                 }
 
                 if (!forecaster.IsWetToday())
                     continue;
 
-                wetDays++;
+                forecastByDay[i] = WeatherDescription.Wet;
 
                 if (forecaster.IsVeryWetToday())
                 {
-                    veryWetDaysRegister.Add(i);
+                    forecastByDay[i] = WeatherDescription.VeryWet;
                 }
             }
+
+            SaveForecast(forecastByDay);
+
+            PrintSummary(forecastByDay);
+
+            Console.ReadLine();
+        }
+
+        static void SaveForecast(Dictionary<int, WeatherDescription> forecastByDay)
+        {
+            var assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var dataPath = Path.Combine(assemblyLocation, "data.txt");
+
+            var weatherData = forecastByDay.Select(day => $"{day.Key}:{day.Value}");
+            File.WriteAllText(dataPath, string.Join(Environment.NewLine, weatherData));
+        }
+
+        static void PrintSummary(Dictionary<int, WeatherDescription> forecastByDay)
+        {
+            Console.WriteLine($"Days with dry weather: " +
+                $"{forecastByDay.Count(days => days.Value == WeatherDescription.Dry)}");
+
+            Console.WriteLine($"Days with wet weather: " +
+                $"{forecastByDay.Count(days => days.Value == WeatherDescription.Wet)}");
+
+            Console.WriteLine($"Days with great weather: " +
+                $"{forecastByDay.Count(days => days.Value == WeatherDescription.Great)}");
+
+            Console.WriteLine($"Days with very wet weather: " +
+                $"{forecastByDay.Count(days => days.Value == WeatherDescription.VeryWet)}");
+
+            Console.WriteLine($"Days with regular weather: " +
+                $"{forecastByDay.Count(days => days.Value == WeatherDescription.None)}");
         }
     }
 }
