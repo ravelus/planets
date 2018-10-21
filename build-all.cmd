e04f6395a871bd5587970218693f35dea3fce6a3
Luis Rodríguez Neches <luis.rodriguez.neches@everis.com>
2018-10-21 12:02:16 -0300

    build-all.cmd

---
+++ build-all.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+cls
+
+echo
+echo
+echo
+echo ----------------------------
+echo Building the application ...
+echo ----------------------------
+
+dotnet build
+
+echo
+echo
+echo
+echo
+echo ---------------------------------
+echo Building the REST API service ...
+echo ---------------------------------
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" PlanetSystemAPI\PlanetSystemAPI.sln
+
+echo
+echo
+echo
+echo
+echo -----------------
+echo Running tests ...
+echo -----------------
+
+pushd .
+
+cd PlanetSystemUnitTests
+
+dotnet test
+
+popd
+
+echo Done.
